e3cfbf4e700a45f21a2a98c98df5aaa7414654f4
alen66 <centr-opt-pavl@yandex.ru>
2020-05-01 09:30:12 +0300

    Завершаем подготовку адаптивной графики в личном проекте

--- source/js/script.js
+++ source/js/script.js
@@ -2,43 +2,32 @@ var open = document.querySelector(".response__btn");
 var modal = document.querySelector(".form-error");
 var close = document.querySelector(".form-error__btn");
 
-
-
 if (open) {open.addEventListener("click", function (evt) {
-
   modal.classList.add("modal-show");
-
 });
-
-
-close.addEventListener("click", function (evt) {
-
-  modal.classList.remove("modal-show");
-
-});
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-
-    if (modal.classList.contains("modal-show")) {
-      modal.classList.remove("modal-show");
-
+  close.addEventListener("click", function (evt) {
+    modal.classList.remove("modal-show");
+  });
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (modal.classList.contains("modal-show")) {
+        modal.classList.remove("modal-show");
+      }
     }
-  }
-});
+  });
 };
 
 
-  var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
-  navMain.classList.remove('main-nav--nojs');
-
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-    }
-  });
+navMain.classList.remove('main-nav--nojs');
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
